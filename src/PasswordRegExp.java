02aae38da99d6d626d859649ffcfaf87d45f44da
maxkru <max_kru@hotmail.com>
2019-11-27 00:55:18 +0300

    Task 2

---
+++ src/PasswordRegExp.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class PasswordRegExp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Password?: ");
+        System.out.println(checkPasswordValidity(scanner.nextLine()));
+    }
+
+    static boolean checkPasswordValidity(String password) {
+        // пароль длиной >= 8, состоит из букв и цифр. Минимум 1 цифра, 1 строчная и 1 прописная
+        return Pattern.matches("^(?=.*\\d)(?=.*\\p{Lower})(?=.*\\p{Upper})[\\w[^_]]{8,}$", password);
+    }
+}
