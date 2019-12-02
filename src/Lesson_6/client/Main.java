b358cbb5e4da263759e59aa5d22fa3da0fff89cd
maxkru <max_kru@hotmail.com>
2019-12-02 17:26:20 +0300

    Task 2: initial code

---
+++ src/Lesson_6/client/Main.java
@@ -0,0 +1,27 @@
+package Lesson_6.client;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import javax.swing.*;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Chat");
+        primaryStage.setScene(new Scene(root, 350, 375));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
