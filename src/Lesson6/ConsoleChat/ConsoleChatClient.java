87ad043328c9db5bf10e167ac50dc5b2741d7d90
maxkru <max_kru@hotmail.com>
2019-12-02 16:52:19 +0300

    Task 1

---
+++ src/Lesson6/ConsoleChat/ConsoleChatClient.java
@@ -0,0 +1,73 @@
+package Lesson6.ConsoleChat;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Scanner;
+
+public class ConsoleChatClient {
+    public static void main(String[] args) {
+        final String IP_ADDRESS = "localhost";
+        final int PORT = 8190;
+        Socket socket = null;
+        DataInputStream in;
+        DataOutputStream out;
+
+        try {
+            System.out.println("Клиент запускается...");
+            socket = new Socket(IP_ADDRESS, PORT);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true) {
+                            String srt = in.readUTF();
+                            if (srt.equals("/serverClosed")){
+                                break;
+                            }
+                        }
+                    } catch (SocketException e) {
+                        System.out.println("Сокет сервера был закрыт.");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+            System.out.println("Подключились к серверу.");
+            String outMsg;
+            Scanner scanner = new Scanner(System.in);
+            while (!(outMsg = scanner.nextLine()).toLowerCase().equals("/end") ) {
+                out.writeUTF(outMsg);
+            }
+
+        } catch (ConnectException e) {
+            System.out.println("Не удалось подключиться к серверу.");
+        } catch (SocketException e) {
+            System.out.println("Отключились от сервера.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Завершаем работу...");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                if(socket != null)
+                    socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
---
+++ src/Lesson6/ConsoleChat/ConsoleChatServer.java
@@ -0,0 +1,77 @@
+package Lesson6.ConsoleChat;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Scanner;
+
+public class ConsoleChatServer {
+    private static final int PORT_NUMBER = 8190;
+
+
+    public static void main(String[] args) {
+        ServerSocket server = null;
+        Socket socket = null;
+        DataInputStream inStream;
+        DataOutputStream outStream;
+
+        try {
+            server = new ServerSocket(PORT_NUMBER);
+            System.out.println("Сервер запущен, порт " + PORT_NUMBER + '.');
+            System.out.println("Ожидаем клиента...");
+            socket = server.accept();
+            inStream = new DataInputStream(socket.getInputStream());
+            outStream = new DataOutputStream(socket.getOutputStream());
+            System.out.println("Клиент подключился.");
+
+            Socket finalSocket = socket;
+            Thread readFromClientThread = new Thread(() -> {
+                String inMsg;
+                try {
+                    while (!(inMsg = inStream.readUTF()).toLowerCase().equals("/end")) {
+                        System.out.println(inMsg);
+                    }
+                    outStream.writeUTF("/serverClosed");
+                } catch (SocketException | EOFException e) {
+                    System.out.println("Сокет клиента был закрыт.");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            readFromClientThread.start();
+
+            String outMsg;
+            Scanner scanner = new Scanner(System.in);
+            while (!(outMsg = scanner.nextLine()).toLowerCase().equals("/end")) {
+                try {
+                    outStream.writeUTF(outMsg);
+                } catch (SocketException e) {
+                    System.out.println("Сокет клиента был закрыт.");
+                }
+            }
+
+            outStream.writeUTF("/serverClosed");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Сервер завершает работу...");
+            try {
+                if (socket != null)
+                    socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (server != null)
+                    server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
